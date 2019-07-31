66447673de3408f73c5c22d18ddd19c0955d9543
Matt Inamdar <matt@ayup.agency>
2019-07-31 14:55:30 +0100

    Added admin-users.index page

--- pages/admin-users/index.vue
+++ pages/admin-users/index.vue
@@ -1,30 +1,112 @@
 <template>
   <gov-width-container>
-    <gov-main-wrapper>
-      <gov-heading-l>Admin Users</gov-heading-l>
+    <gov-breadcrumbs :items="breadcrumbs" />
 
-      <gov-body>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-        voluptate. Quae eius, fuga excepturi enim quam veritatis quidem nesciunt
-        omnis aliquam dicta provident laboriosam alias suscipit? Soluta ducimus
-        incidunt delectus?
-      </gov-body>
+    <gov-main-wrapper>
+      <gov-grid-row>
+        <gov-grid-column-two-thirds>
+          <gov-heading-l>Admin Users</gov-heading-l>
+
+          <gov-body>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+            voluptate. Quae eius, fuga excepturi enim quam veritatis quidem
+            nesciunt omnis aliquam dicta provident laboriosam alias suscipit?
+            Soluta ducimus incidunt delectus?
+          </gov-body>
+        </gov-grid-column-two-thirds>
+      </gov-grid-row>
+
+      <gov-grid-row>
+        <gov-grid-column-full>
+          <custom-resource-table
+            :columns="columns"
+            :model="model"
+            default-sort="name"
+          >
+            <template v-slot:0="{ resource: admin }">
+              {{ admin.name }}
+            </template>
+
+            <template v-slot:1="{ resource: admin }">
+              {{ admin.email }}
+            </template>
+
+            <template v-slot:2="{ resource: admin }">
+              {{ admin.phone }}
+            </template>
+
+            <template v-slot:actions="{ resource: admin }">
+              <gov-link
+                :url="{
+                  name: 'admin-users-show',
+                  params: { admin_user: admin.id }
+                }"
+                no-visited-state
+              >
+                View
+              </gov-link>
+            </template>
+          </custom-resource-table>
+        </gov-grid-column-full>
+      </gov-grid-row>
     </gov-main-wrapper>
   </gov-width-container>
 </template>
 
 <script>
+import CustomResourceTable from '~/components/custom/ResourceTable.vue'
 import GovBody from '~/components/gov/Body.vue'
+import GovBreadcrumbs from '~/components/gov/Breadcrumbs.vue'
+import GovGridColumnFull from '~/components/gov/GridColumnFull.vue'
+import GovGridColumnTwoThirds from '~/components/gov/GridColumnTwoThirds.vue'
+import GovGridRow from '~/components/gov/GridRow.vue'
 import GovHeadingL from '~/components/gov/HeadingL.vue'
+import GovLink from '~/components/gov/Link.vue'
 import GovMainWrapper from '~/components/gov/MainWrapper.vue'
 import GovWidthContainer from '~/components/gov/WidthContainer.vue'
+import Admin from '~/models/Admin'
 
 export default {
   components: {
+    CustomResourceTable,
     GovBody,
+    GovBreadcrumbs,
+    GovGridColumnFull,
+    GovGridColumnTwoThirds,
+    GovGridRow,
     GovHeadingL,
+    GovLink,
     GovMainWrapper,
     GovWidthContainer
+  },
+
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Dashboard',
+          url: { name: 'index' }
+        },
+        {
+          text: 'Admin Users'
+        }
+      ],
+      columns: [
+        {
+          heading: 'Name',
+          sort: 'name'
+        },
+        {
+          heading: 'Email',
+          sort: 'email'
+        },
+        {
+          heading: 'Phone',
+          sort: 'phone'
+        }
+      ],
+      model: Admin
+    }
   }
 }
 </script>
